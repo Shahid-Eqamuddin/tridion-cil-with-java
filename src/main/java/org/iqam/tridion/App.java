acc565f4474d429e16a146fd277eea5558676a12
Shahid-Eqamuddin <34349798+Shahid-Eqamuddin@users.noreply.github.com>
2017-12-10 13:47:04 +0100

    first commit

---
+++ src/main/java/org/iqam/tridion/App.java
@@ -0,0 +1,17 @@
+package org.iqam.tridion;
+
+import com.tridion.dynamiccontent.ComponentPresentationAssembler;
+
+/**
+ * Hello world with Tridion CIL!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {        
+        ComponentPresentationAssembler cpa = new ComponentPresentationAssembler(11);        
+        String result = cpa.getContent("tcm:11-645", "tcm:11-643-32");
+        System.out.println( "cpa output is!:"+result);
+    }
+}
